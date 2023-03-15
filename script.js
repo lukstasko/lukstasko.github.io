fe08b4a6fb392bfd5ec73c5037dc1d6d9be2d020
lukstasko <119363165+lukstasko@users.noreply.github.com>
2023-03-15 23:46:43 +0100

    Update 1

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  const apiRoot = 'http://localhost:8080/v1/tasks';
-  const trelloApiRoot = 'http://localhost:8080/v1/trello';
+  const apiRoot = 'https://kodilla-java-2.lukstasko.repl.co/v1/tasks';
+  const trelloApiRoot = 'https://api.trello.com/1/trello';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
